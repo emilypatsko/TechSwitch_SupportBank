4aa2883607a6c77482f52b9b9de3409a718941e6
Emily Patsko <Emily.Patsko@softwire.com>
2021-09-22 16:29:28 +0100

    Part 2: List all accounts

--- Program.cs
+++ Program.cs
@@ -10,18 +10,10 @@ namespace SupportBank
         static void Main()
         {
             var transactions = ReadCsvFile(@"Transactions2014.csv");
-
-            foreach (var transaction in transactions)
-            {
-                Console.WriteLine("Date: {0} From: {1} To: {2} Narrative: {3} Amount: {4}",
-                                    transaction.Date.ToShortDateString(),
-                                    transaction.From,
-                                    transaction.To,
-                                    transaction.Narrative,
-                                    transaction.Amount);
-            }
+            var accounts = CreateAccountsFromTransactions(transactions);
+            
+            ListAllAccounts(accounts);
         }
-
         
         private static IEnumerable<Transaction> ReadCsvFile(string filename)
         {
@@ -41,5 +33,46 @@ namespace SupportBank
                 };
             }
         }       
+    
+        private static Dictionary<string, Account> CreateAccountsFromTransactions(IEnumerable<Transaction> transactions)
+        {
+            var accounts = new Dictionary<string, Account>();
+
+            foreach (var transaction in transactions)
+            {
+                GetOrCreateAccount(accounts, transaction.From).OutgoingTransactions.Add(transaction);
+                GetOrCreateAccount(accounts, transaction.To).IncomingTransactions.Add(transaction);
+            }
+
+            return accounts;
+        }
+
+        private static Account GetOrCreateAccount(Dictionary<string, Account> accounts, string name)
+        {
+            if (accounts.ContainsKey(name))
+            {
+                return accounts[name];
+            }
+
+            var newAccount = new Account(name);
+            accounts[name] = newAccount;
+            return newAccount;
+        }
+   
+        private static void ListAllAccounts(Dictionary<string, Account> accounts)
+        {
+            foreach (var account in accounts.Values)
+            {
+                var netBalance = account.IncomingTransactions.Sum(tr => tr.Amount) -
+                                 account.OutgoingTransactions.Sum(tr => tr.Amount);
+
+                Console.WriteLine("{0,-11}{1,-10}{2,-10}",
+                                account.Name,
+                                (netBalance < 0 ? "owes" : "is owed"),
+                                $"{Math.Abs(netBalance):C}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
